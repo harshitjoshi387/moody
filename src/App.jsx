798ae61419da485115ba318bfa73c7595d2fbd0d
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-03 19:07:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MoodMusic from "./components/MoodMusic";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        🎵 Mood Based Music Suggestion App
+      </h1>
+      <MoodMusic />
+    </div>
+  );
+}
+
+export default App;
