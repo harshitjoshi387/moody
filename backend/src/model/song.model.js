57ea9d6ddf3ed106f022f910b7604b1445ca1dd0
Harshit joshi <harshjoshi25022002@gmail.com>
2026-04-21 23:13:46 +0530

    feat: Update mood field to include enum values for song moods

--- backend/src/model/song.model.js
+++ backend/src/model/song.model.js
@@ -4,7 +4,7 @@ const songSchema = new mongoose.Schema({
   title: { type: String, required: true },
   artist: { type: String, required: true },
   genre: { type: String, required: true },
-  mood: { type: String, required: true },
+  mood: { type: String, required: true,enum:{["sad,"happy",'surprised']} },
   url: { type: String, required: true },
   posterUrl:{
     type:String,
